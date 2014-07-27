4315c9e500c706c9fae80946a85131a83a1a6231
Lyubov Los <lyubov.los@gmail.com>
2014-07-27 11:03:32 +0300

    Redone registration code.

--- js/list.js
+++ js/list.js
@@ -14,63 +14,60 @@
 
 var arrayTasks = [];
 
-function addElement()
+window.onload = function()
 {
-    var item = document.getElementById("list-input");
-    var item_value = item.value;
-    if (item_value.trim() != "")
-    {
-        item.value = "";
-        addToArray(item_value);
-    }
+    document.getElementById('list-add').onclick = addElement;
 };
 
-function rendering()
-{
-    var externDiv = document.createElement("div");
-    externDiv.id = "parent-task";
-    externDiv.onclick = function(e)
-    {
-        e = e || window.event;
-        var target = e.target || e.srcElement;
-        if (target.className != "list-image-delete")
-        {
-            return;
-        }
-        removeElement(target.parentNode.id);
-    }
-    for(var i in arrayTasks)
-    {
-        var taskDiv = document.createElement("div");
-		taskDiv.id = i;
-        taskDiv.className = "task-div";
-        var p = document.createElement("xmp");
-        p.className = "task-content";
-        p.textContent = parseInt(i) + 1 + ". " + arrayTasks[i];
-        taskDiv.appendChild(p);
-        var del = document.createElement("img");
-        del.className = "list-image-delete";
-        del.src = "images/Sys_Error.png";
-        taskDiv.appendChild(del);
-        externDiv.appendChild(taskDiv);
-    }
-    var oldTasks = document.getElementById("parent-task");
-    var listDiv = document.getElementById("list-div");
-    listDiv.replaceChild(externDiv, oldTasks);
-}
 
-function addToArray(element)
+function addElement()
 {
-    arrayTasks.push(element);
-    rendering();
+    var item = document.getElementById('list-input');
+    var itemValue = item.value;
+    if (itemValue.trim() !== '')
+    {
+        item.value = '';
+        arrayTasks.push(itemValue);
+        rendering();
+    }
 }
 
 function removeElement(id)
 {
     arrayTasks.splice(id, 1);
-    rendering()
+    rendering();
 }
 
-window.onload=function(){
-	document.getElementById("list-add").onclick = addElement;
+function rendering()
+{
+    var externDiv = document.createElement('div');
+    externDiv.id = 'parent-task';
+    externDiv.onclick = function(e)
+    {
+        e = e || window.event;
+        var target = e.target || e.srcElement;
+        if (target.className != 'list-image-delete')
+        {
+            return;
+        }
+        removeElement(target.parentNode.id);
+    };
+    for (var i in arrayTasks)
+    {
+        var taskDiv = document.createElement('div');
+        taskDiv.id = i;
+        taskDiv.className = 'task-div';
+        var p = document.createElement('xmp');
+        p.className = 'task-content';
+        p.textContent = parseInt(i) + 1 + '. ' + arrayTasks[i];
+        taskDiv.appendChild(p);
+        var del = document.createElement('img');
+        del.className = 'list-image-delete';
+        del.src = 'images/Sys_Error.png';
+        taskDiv.appendChild(del);
+        externDiv.appendChild(taskDiv);
+    }
+    var oldTasks = document.getElementById('parent-task');
+    var listDiv = document.getElementById('list-div');
+    listDiv.replaceChild(externDiv, oldTasks);
 }
